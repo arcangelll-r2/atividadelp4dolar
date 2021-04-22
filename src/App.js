07b97f07a69a2b765a8a60c6d13503db0904fa81
arcangelll-r2 <joaobarbosa014@gmail.com>
2021-04-22 00:24:26 -0300

    atividade

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Título">
+        Real e Dólar
+      </div>
+      <h1>
+      </h1>
+      <div className="Real">
+        Real
+        <div><input className="entrada1" /></div>
+      </div>
+      <div className="Dolar">
+        Dólar
+        <div><input className="entrada2" /></div>
+      </div>
+      <h1>
+      </h1>
+      <div className="Botao">
+        Converter
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
